4183be4ee41241ab9f93fa6504073e2f1b865a30
vchastinet <victorchastinet@live.com>
2018-11-18 22:46:47 -0300

    updating queries for new tagging system

--- queries.sql
+++ queries.sql
@@ -7,19 +7,16 @@ INSERT INTO users (email, name, password) VALUES ($1, $2, $3);
 -- * --
 
 -- GET ALL ARTISTS
-SELECT a.id, a.name, a.instagram_username, a.instagram_url, a.url, t.name AS tags, uf.nome AS estado, uf.uf AS sigla
+SELECT a.id, a.name, a.instagram_username, a.instagram_url, a.url, uf.nome AS estado, uf.uf AS sigla
 FROM artist AS a
        INNER JOIN estado AS uf ON uf.id = a.estado
-       LEFT JOIN artists_tags AS a_t ON a_t.artist_id = a.id
-       LEFT JOIN tags t on a_t.tag_id = t.id;
 -- * --
 
 -- GET ALL ARTISTS BY UF
-SELECT a.id, a.name, a.instagram_username, a.instagram_url, a.url, t.name AS tags, uf.nome AS estado, uf.uf AS sigla
+SELECT a.id, a.name, a.instagram_username, a.instagram_url, a.url, uf.nome AS estado, uf.uf AS sigla
 FROM artist AS a
        INNER JOIN estado AS uf ON uf.id = a.estado
-       LEFT JOIN artists_tags AS a_t ON a_t.artist_id = a.id
-       LEFT JOIN tags t on a_t.tag_id = t.id WHERE uf.id = $1;
+       WHERE uf.id = $1;
 -- * --
 
 -- GET ALL ARTISTS BY TAGS
@@ -27,7 +24,8 @@ SELECT a.id, a.name, a.instagram_username, a.instagram_url, a.url, t.name AS tag
 FROM artist AS a
        INNER JOIN estado AS uf ON uf.id = a.estado
        LEFT JOIN artists_tags AS a_t ON a_t.artist_id = a.id
-       LEFT JOIN tags t on a_t.tag_id = t.id WHERE t.id = $1;
+       LEFT JOIN tags t on a_t.tag_id = t.id
+       WHERE t.id = $1;
 -- * --
 
 -- GET ARTIST BY NAME
@@ -37,6 +35,14 @@ FROM artist AS a
        LEFT JOIN artists_tags AS a_t ON a_t.artist_id = a.id
        LEFT JOIN tags t on a_t.tag_id = t.id WHERE LOWER(a.name) LIKE LOWER('%$1%');
 -- * --
+
+-- GET ARTIST'S TAGS
+SELECT t.*
+FROM tags AS t
+       INNER JOIN artists_tags AS a_t ON a_t.tag_id = t.id
+       LEFT JOIN artist AS a on a_t.artist_id = a.id
+WHERE a.id = 1;
+-- *--
 
 -- UPDATE ARTIST
 UPDATE artist SET (name, instagram_url, instagram_username, url, estado) = ($1, $2, $3, $4, $5) WHERE id = $6;
